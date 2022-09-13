822f34bd53d58829a89571316953b5d4fdce8e8f
anasa <anasa@DESKTOP-AOG59KC>
2022-09-13 07:02:06 +0200

    Add project files.

---
+++ Interfaces/IUnit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IMSITI.Models;
+using CodesByAniz.Tools;
+using IMSITI.Models;
+using InventoryBeginners.Models;
+
+namespace IMSITI.Interfaces
+{
+    public interface IUnit
+    {
+        PaginatedList<Unit> GetItems(string SortProperty, SortOrder sortOrder, string SearchText = "", int pageIndex = 1, int pageSize = 5); //read all
+        Unit GetUnit(int id); // read particular item
+
+        Unit Create(Unit unit);
+
+        Unit Edit(Unit unit);
+
+        Unit Delete(Unit unit);
+
+        public bool IsUnitNameExists(string name);
+        public bool IsUnitNameExists(string name, int Id);
+
+
+    }
+}
